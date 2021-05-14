f4c5686a9a032e03a1e7182d7ea5b396a36e9895
vivekkumar333 <tiwari.vivekkumar333@gmail.com>
2021-05-14 16:07:02 +0530

    Default initial commit

---
+++ src/main/java/com/skyx/gateway/SkyxApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.skyx.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SkyxApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyxApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skyx/gateway/SkyxApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyx.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyxApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
